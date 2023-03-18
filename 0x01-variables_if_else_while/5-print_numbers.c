870ba152292a7724c9888c96f7787a09e83506f3
Souhail Ouaggague <souhailouaggague@gmail.com>
2023-03-17 19:51:14 -0700

    new directory

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
